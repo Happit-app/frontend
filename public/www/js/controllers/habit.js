2223341539add20de3d72419f4bf536e2723a331
Alex Southwell <abkruse@gmail.com>
2016-06-15 10:51:37 -0700

    Time picker fires on click of button

--- public/www/js/controllers/habit.js
+++ public/www/js/controllers/habit.js
@@ -17,20 +17,20 @@ function HabitCtrl(HabitsServices, ionicTimePicker) {
 
   }
 
-  var ipObj1 = {
-    callback: function (val) {
-      if(typeof (val) === 'undefined') {
-        console.log('Time not selected');
-      } else {
-        var selectedTime = new Date(val * 1000);
-        console.log('Seleced epoch : ', val, 'and the time is', selectedTime.getUTCHours(), 'H :', selectedTime.getUTCMinutes(), 'M');
-      }
-    },
-    inputTime: 50400,
-    format: 12,
-    step: 15,
-    setLabel: 'Set2'
-  };
-
-  ionicTimePicker.openTimePicker(ipObj1);
+  this.openTimePicker = function() {
+      var ipObj1 = {
+        callback: function (val) {
+          if (typeof (val) === 'undefined') {
+            console.log('Time not selected');
+          } else {
+            var selectedTime = new Date(val * 1000);
+            console.log('Selected epoch is : ', val, 'and the time is ', selectedTime.getUTCHours(), 'H :', selectedTime.getUTCMinutes(), 'M');
+          }
+        },
+        inputTime: 50400,
+        format: 12,
+        setLabel: 'Set'
+      };
+      ionicTimePicker.openTimePicker(ipObj1);
+    };
 }
