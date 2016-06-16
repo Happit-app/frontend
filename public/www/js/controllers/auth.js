e45eefc576361f428fb41ed2ea4f13480cf45b32
Max Dobler <mdblr@outlook.com>
2016-06-15 17:08:39 -0700

    exchange access token for user object

--- public/www/js/controllers/auth.js
+++ public/www/js/controllers/auth.js
@@ -2,37 +2,29 @@ angular
   .module('happit')
     .controller('AuthCtrl', AuthCtrl)
 
-AuthCtrl.$inject = ['$cordovaOauth']
+AuthCtrl.$inject = ['$cordovaOauth', '$http']
 
-function AuthCtrl($cordovaOauth) {
+function AuthCtrl($cordovaOauth, $http) {
 
   var ctrl = this;
-  var google_id = '';
-  var url_short = 'https://www.googleapis.com/auth/urlshortener';
-  var google_auth = 'https://www.googleapis.com/auth/userinfo.email';
   var facebook_id = '';
-
-
-  ctrl.googleLogin = function() {
-    $cordovaOauth
-      .google(google_id, [url_short , google_auth])
-        .then(function(result) {
-          console.log(JSON.stringify(result));
-        }, function(error) {
-          console.log(JSON.stringify(error));
-        });
+  var fb = 'https://graph.facebook.com/v2.5/me?fields=name,email';
+  var tokenXc = function(endpoint, token) {
+    $http.get(endpoint, {params: {access_token: token, format: "json" }})
+    .then(function(user) {
+      console.log(user);
+    });
   }
 
   ctrl.fbLogin = function() {
     $cordovaOauth
       .facebook(facebook_id, ['email','public_profile'], {"auth_type": "rerequest"})
         .then(function(result) {
-          console.log(JSON.stringify(result));
+            tokenXc(fb, result.access_token)
         }, function(error) {
           console.log(JSON.stringify(error));
         });
   }
-
 
   this.service = AuthServices;
 }
