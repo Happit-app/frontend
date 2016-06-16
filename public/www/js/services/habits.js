0f2ed7df0933137e95cf775c0d8d1f276ba41276
Devany Moe <devanymoe@gmail.com>
2016-06-15 17:29:28 -0700

    Update calendar

--- public/www/js/services/habits.js
+++ public/www/js/services/habits.js
@@ -2,16 +2,23 @@ angular
   .module("happit")
   .factory("HabitsServices", HabitsServices)
 
-HabitsServices.$inject = []
+HabitsServices.$inject = ['$http']
 
-function HabitsServices() {
+function HabitsServices($http) {
+  var api = 'http://localhost:3000/';
 
   return {
     completeTask: function(id) {
 
     },
     incompleteTask: function(id) {
-      
+
+    },
+    getHabit: function(id) {
+      console.log(id);
+      return $http.get(api + '/habits/' + id).then(function(data) {
+        return data;
+      });
     }
   }
 };
