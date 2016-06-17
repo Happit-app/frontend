58ed3efb55331d68eea4af0a420cdd1b3f461863
Max Dobler <mdblr@outlook.com>
2016-06-16 17:04:03 -0700

    merge upstream

--- public/www/js/app.js
+++ public/www/js/app.js
@@ -6,47 +6,55 @@
     .config(routeHandler)
     .run(runBlock);
 
-    routeHandler.$inject = ['$stateProvider', '$urlRouterProvider', '$locationProvider'];
-    runBlock.$inject = ['$ionicPlatform'];
+    routeHandler.$inject = ['$stateProvider', '$urlRouterProvider', '$locationProvider', '$httpProvider']
+    runBlock.$inject = ['$ionicPlatform', '$rootScope', '$location', '$window'];
 
-    function routeHandler($stateProvider, $urlRouterProvider, $locationProvider) {
+
+    function routeHandler($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
       $urlRouterProvider.otherwise('/');
 
       $stateProvider
-        .state('home', {
-          url: '/',
-          templateUrl: 'partials/home.html',
-          controller: 'HabitsCtrl as ctrl'
-        })
         .state('login', {
-          url: '/login',
+          url: '/',
           templateUrl: 'partials/login.html',
-          controller: 'AuthCtrl as ctrl'
+          controller: 'AuthCtrl as ctrl',
+          preventWhenLoggedIn: true
+        })
+        .state('home', {
+          url: '/home',
+          templateUrl: 'partials/home.html',
+          controller: 'HabitsCtrl as ctrl',
+          restricted: true
         })
         .state('settings', {
           url: '/settings',
           templateUrl: 'partials/settings.html',
-          controller: 'UserCtrl as ctrl'
+          controller: 'UserCtrl as ctrl',
+          restricted: true
         })
         .state('habit', {
           url: '/habit/:id',
           templateUrl: 'partials/habit.html',
           controller: 'HabitCtrl as ctrl',
+          restricted: true
         })
         .state('new', {
           url: '/new',
           templateUrl: 'partials/new.html',
-          controller: 'HabitCtrl as ctrl'
+          controller: 'HabitCtrl as ctrl',
+          restricted: true
         })
         .state('edit', {
           url: '/edit',
           templateUrl: 'partials/edit.html',
-          controller: 'HabitCtrl as ctrl'
+          controller: 'HabitCtrl as ctrl',
+          restricted: true
         })
       // $locationProvider.html5Mode(true);
+      $httpProvider.interceptors.push('AuthInterceptor');
     }
 
-    function runBlock($ionicPlatform) {
+    function runBlock($ionicPlatform, $rootScope, $location, $window) {
       $ionicPlatform.ready(function() {
         if(window.cordova && window.cordova.plugins.Keyboard) {
           cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
@@ -55,6 +63,19 @@
         if(window.StatusBar) {
           StatusBar.styleDefault();
         }
+      });
+
+      $rootScope.$on('$routeChangeStart', function (event, next, current) {
+        if (next.restricted && !$window.localStorage.getItem("token")) {
+            $location.path('/login');
+        }
+        else if (next.preventWhenLoggedOut && !$window.localStorage.getItem("token")) {
+            $location.path('/login');
+        }
+        else if (next.preventWhenLoggedIn && $window.localStorage.getItem("token")) {
+          $location.path('/members');
+        }
+
       });
     }
 
